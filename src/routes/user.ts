334e42032fd6691c8ba77793112e7c07baaa1d01
manuelosorio <manuelosorio.mkdesignz@gmail.com>
2020-05-04 12:08:27 -0400

    Add post method to retrieve form data from frontend server

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,14 +1,20 @@
 import Router from 'express';
 import db from '../database/db';
+import bodyParser from 'body-parser';
+import {variables} from '../environments/variables';
 
 const userRoutes = Router();
 const connection = db;
 const users = "SELECT * FROM users";
 const individualUser = "SELECT * FROM users WHERE username = ?";
 
+userRoutes.use(bodyParser.json());
+userRoutes.use(bodyParser.urlencoded({extended: false}))
+
+
 userRoutes.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 userRoutes.get('/users', (req, res) => {
@@ -42,5 +48,27 @@ userRoutes.get('/users/:username', (req, res, next) => {
     return user;
   })
 })
+
+userRoutes.post('/new/user', (req, res, next) => {
+  console.log('new user');
+  const firstName: string = req.body.firstName;
+  const lastName: string = req.body.lastName;
+  const email: string = req.body.email;
+  const username: string = req.body.username;
+  const password: string = req.body.password;
+
+  connection.query(variables.postQuery, [firstName, lastName, email, username, password, 0, 0],
+    (err, results, fields) => {
+      if (err) {
+        console.log(req);
+        throw err.message;
+        res.sendStatus(500);
+        return;
+      }
+      res.end();
+    });
+});
+
+
 
 export default userRoutes;
