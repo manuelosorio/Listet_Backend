06749b74455ba7dd3266063a9404ad1c8c2bfa47
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2021-04-06 03:38:14 -0400

    Small Changes

--- src/utilities/date.ts
+++ src/utilities/date.ts
@@ -11,7 +11,7 @@ export class DateUtil{
     const month = this.months[date.getMonth()];
     const day = date.getDate();
     const year = date.getFullYear();
-    return month + " " + day + ", " + year;
+    return `${month} ${day}, ${year}`;
   }
   getMonth(): number {
     return new Date(this.date).getMonth() + 1;
