manuelosorio/Listet_Backend

commit c07dec0e042b33f9ab5009813e7fdea5fa3b0aae

Add auth and list related middleware, remove unneeded conditional from item controller.

src/api/lists.api.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import { ItemController } from '../controllers/list/item/item.controller';
 import { CommentController } from '../controllers/list/comment/comment.controller';
 import { ListController } from '../controllers/list/list.controller';
+import { isAuth } from '../middleware/auth.middleware';
+import { checkListTitle, isItemEmpty, isListOwner } from '../middleware/list.middleware';
 
 const listApi = Router();
 const listController = new ListController();
@@ -17,10 +19,10 @@ listApi.put('/update-list/:id', checkListTitle, listController.update);
 listApi.delete('/delete-list/:id', listController.delete);
 /********** Items *************/
 listApi.get('/list/:slug/items', itemController.get);
-listApi.post('/add-item', itemController.post);
-listApi.delete('/delete-item/:id', isAuthed, itemController.delete);
-listApi.put('/update-item-status', itemController.updateStatus);
-listApi.put('/update-item/:id', itemController.update);
+listApi.post('/add-item', isAuth, isListOwner, isItemEmpty, itemController.post);
+listApi.delete('/delete-item/:id', isAuth, isListOwner, itemController.delete);
+listApi.put('/update-item-status', isAuth, isListOwner, itemController.updateStatus);
+listApi.put('/update-item/:id', isAuth, isListOwner, isItemEmpty, itemController.update);
 /********** Comments **********/
 listApi.get('/list/:slug/comments', commentController.get);
 listApi.post('/create-comment', commentController.post);
@@ -28,16 +30,3 @@ listApi.put('/update-comment/:id', commentController.update);
 listApi.delete('/delete-comment/:id', commentController.delete);
 export default listApi;
 
-function isAuthed(req, res, next) {
-  if (req.session.user) {
-    return next();
-  }
-  return res.status(403).send({message: 'You must be authenticated to complete that action.'})
-}
-function checkListTitle(req, res, next) {
-  console.log(req.body.title.length)
-  if (req.body.title.length > 0) {
-    return next();
-  }
-  return res.status(400).send({ message: "Title can't be empty"})
-}

src/middleware/list.middleware.ts
@@ -1,0 +1,25 @@
+import { NextFunction, Request, Response } from 'express';
+import { ListService } from '../services/list.service';
+
+export function checkListTitle(req: Request, res: Response, next: NextFunction): void | Response {
+  console.log(req.body.title.length)
+  if (req.body.title.length > 0) {
+    return next();
+  }
+  return res.status(400).send({ message: "Title can't be empty" });
+}
+
+export function isItemEmpty(req: Request, res: Response, next: NextFunction): void | Response  {
+  if (req.body.item.length < 1) return res.status(403).send({message: "Item Can't be empty."});
+  return next()
+}
+
+export const isListOwner = async (req: Request, res: Response, next: NextFunction): Promise<void | Response> => {
+  const listService = new ListService();
+  const isOwner = await listService.isListOwner(req.session.user[0].id, req.body.list_id);
+  console.log("is owner:",isOwner)
+  if (isOwner) {
+    return next();
+  }
+  return res.status(400).send({message: "You don't have permission to add items to this list."}).end();
+}

src/controllers/list/item/item.controller.ts
@@ -30,7 +30,6 @@ export class ItemController {
   post = async (req: Request, res: Response): Promise<Query | void | Response> => {
     const id = Number(req.body.list_id);
     const date = req.body.deadline ? req.body.deadline : null;
-    if (req.body.item.length < 1) return res.status(403).send({message: "Item Can't be empty."});
     const listItem: ListItemModel = {
       id: 0,
       item: req.body.item,
@@ -39,94 +38,49 @@ export class ItemController {
       list_id: id,
       slug: req.body.slug
     }
-    if (req.session.user) {
-      const userId = req.session.user[0].id;
-      const isOwner = await this.listService.isListOwner(userId, id);
-      if (isOwner) return this.db.addListItem(listItem,
-        (err, results, _fields) => {
-          if (err) {
-            console.error(err);
-            return res.status(500).end();
-          }
-          listItem.id = results.insertId;
-          emit(ListItemEvents.ADD_ITEM, listItem);
-          return res.status(201).send({message: 'List item added.'});
-        });
-      return res.status(400).send({message: "You don't have permission to add items to this list."}).end();
-    }
-    return res.status(400).send({message: "You must be authenticated to add items to a list."})
+    return this.db.addListItem(listItem, (err, results, _fields) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).end();
+      }
+      listItem.id = results.insertId;
+      emit(ListItemEvents.ADD_ITEM, listItem);
+      return res.status(201).send({message: 'List item added.'});
+    });
   }
   delete = (req: Request, res: Response): Promise<Query> =>  {
     const id = req.params.id as unknown as number;
-    const userID = req.session.user[0].id;
-    console.log(req.session)
-    return this.db.getListItemOwner(id, ((err, result) => {
-      if (err) {
-        console.error(err.message);
+    return this.db.deleteListItem(id, (error: MysqlError) => {
+      if (error) {
+        console.error(error.message);
         return res.status(500).end();
       }
-      if (userID === result[0].owner_id) {
-        this.db.deleteListItem(id, (error: MysqlError) => {
-          if (error) {
-            console.error(error.message);
-            return res.status(500).end();
-          }
-          emit(ListItemEvents.DELETE_ITEM, id);
-          return res.send({message: 'Item Deleted'}).status(202);
-        }).then();
-      }
-    }))
+      emit(ListItemEvents.DELETE_ITEM, id);
+      return res.send({message: 'Item Deleted'}).status(202);
+    });
   }
   updateStatus = (req: Request, res: Response): Promise<Query> | Response => {
     const listItem: ListItemModel = req.body;
-    if (req.session.user) {
-      const userID = req.session.user[0].id;
-      return this.listDb.getListOwner(listItem.list_id, (error, result) => {
-        if (error) {
-          console.error(error)
-          return res.status(500).end();
-        }
-        if (userID === result[0].owner_id) {
-          return this.db.updateListItemStatus({
-            completed: listItem.completed,
-            id: listItem.id
-          }, (err, _) => {
-            if (err) {
-              console.error(err);
-              return res.status(500).end();
-            }
-            emit(ListItemEvents.COMPLETE_ITEM, listItem);
-            return res.status(201).send({ message: "Updated Item Status" }).end();
-          })
-        }
-      })
-    }
-    return res.status(403).send({message: 'You must be authenticated to complete this action. '})
+    return this.db.updateListItemStatus(listItem, (err, _) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).end();
+      }
+      emit(ListItemEvents.COMPLETE_ITEM, listItem);
+      return res.status(201).send({ message: "Updated Item Status" }).end();
+    })
   }
   update = async (req: Request, res: Response, _next: NextFunction): Promise<Query | Response | void> => {
     const listItem: ListItemModel = req.body;
     listItem.deadline = new Date(req.body.deadline);
     listItem.id = req.params.id as unknown as number;
-    if (req.body.item.length < 1) return res.status(403).send({message: "Item Can't be empty."});
-    if (req.session.user) {
-      const userID = req.session.user[0].id;
-      return this.listDb.getListOwner(listItem.list_id, (error: MysqlError, result) => {
-        if (error) {
-          console.error(error)
+      return this.db.updateListItem(listItem, (updateErr: MysqlError, _) => {
+        if (updateErr) {
+          console.error(updateErr)
           return res.status(500).end();
         }
-        if (userID === result[0].owner_id) {
-          return this.db.updateListItem(listItem, (updateErr: MysqlError, _) => {
-            if (updateErr) {
-              console.error(updateErr)
-              return res.status(500).end();
-            }
-            emit(ListItemEvents.UPDATE_ITEM, listItem);
-            return res.status(201).send({ message: "Item Updated" }).end();
-          });
-        }
+        emit(ListItemEvents.UPDATE_ITEM, listItem);
+        return res.status(201).send({ message: "Item Updated" }).end();
       });
-    }
-    res.status(403).send({message: 'You must be authenticated to complete this action.'})
   }
 }

src/database/list/item/list-item.db.ts
@@ -31,7 +31,7 @@ export class ListItemDb extends Db{
         WHERE slug= ?`,
       slug, next);
   }
-  updateListItemStatus = async (listItem: {completed: number, id: number}, next: queryCallback): Promise<Query> => {
+  updateListItemStatus = async (listItem: ListItemModel, next: queryCallback): Promise<Query> => {
     return this.db.query(
       `UPDATE list_items SET completed = ? WHERE id = ? `,
       [

src/middleware/auth.middleware.ts
@@ -1,0 +1,8 @@
+import { NextFunction, Request, Response } from 'express';
+
+export function isAuth(req: Request, res: Response, next: NextFunction): void | Response {
+  if (req.session.user) {
+    return next();
+  }
+  return res.status(403).send({message: 'You must be authenticated to complete that action.'})
+}

commit 8ad3fa5ed4adc24377fdfc23cd1d52b326ebaae0

Fix item deletion bug
This bug was introduced in commit: c07dec0e042b33f9ab5009813e7fdea5fa3b0aae

src/api/lists.api.ts
@@ -3,7 +3,7 @@ import { ItemController } from '../controllers/list/item/item.controller';
 import { CommentController } from '../controllers/list/comment/comment.controller';
 import { ListController } from '../controllers/list/list.controller';
 import { isAuth, isVerified } from '../middleware/auth.middleware';
-import { checkListTitle, isItemEmpty, isListOwner } from '../middleware/list.middleware';
+import { checkListTitle, isItemEmpty, isListItemOwner, isListOwner } from '../middleware/list.middleware';
 import { canDeleteComment, isCommentBodyEmpty, isCommentOwner } from '../middleware/comment.middleware';
 
 const listApi = Router();
@@ -21,7 +21,7 @@ listApi.delete('/delete-list/:id', isAuth, isListOwner, listController.delete);
 /********** Items *************/
 listApi.get('/list/:slug/items', itemController.get);
 listApi.post('/add-item', isAuth, isListOwner, isItemEmpty, itemController.post);
-listApi.delete('/delete-item/:id', isAuth, isListOwner, itemController.delete);
+listApi.delete('/delete-item/:id', isAuth, isListItemOwner, itemController.delete);
 listApi.put('/update-item-status', isAuth, isListOwner, itemController.updateStatus);
 listApi.put('/update-item/:id', isAuth, isListOwner, isItemEmpty, itemController.update);
 /********** Comments **********/

src/database/list/list.db.ts
@@ -20,6 +20,18 @@ export class ListDb extends Db{
     )
   }
   /**
+   * Check if user Owns list
+   * @param itemID
+   * @param next
+   */
+  getListItemOwner = async (itemID: number | any, next: queryCallback): Promise<Query> => {
+    return this.db.query(
+      'SELECT `owner_id` FROM view_list_items WHERE id = ?',
+      itemID,
+      next
+    )
+  }
+  /**
    * Retrieve all public lists.
    * @param next
    */

src/middleware/list.middleware.ts
@@ -20,6 +20,14 @@ export const isListOwner = async (req: Request, res: Response, next: NextFunctio
   }
   return res.status(400).send({message: "You don't have permission to complete that action."}).end();
 }
+export const isListItemOwner = async (req: Request, res: Response, next: NextFunction): Promise<void | Response> => {
+  const itemID = req.params.id as unknown as number;
+  const isOwner = await listService.isListItemOwner(req.session.user[0].id, itemID);
+  if (isOwner) {
+    return next();
+  }
+  return res.status(400).send({message: "You don't have permission to complete that action."}).end();
+}
 
 export const doesListExist = async(req: Request, res: Response, next: NextFunction): Promise<void | Response> => {
   listService.testSlug(req.body.slug).then((r) => {

src/services/list.service.ts
@@ -14,9 +14,27 @@ export class ListService {
 
   public isListOwner = async (userID: number, listID: number): Promise<boolean> => {
     return new Promise((resolve, _reject) => {
+      console.log(listID);
       return this.listDb.getListOwner(listID, (err, results) => {
-        const listOwnerID = results[0].owner_id as unknown as number;
-        return resolve(listOwnerID === userID);
+        try {
+          const listOwnerID = results[0].owner_id as unknown as number;
+          return resolve(listOwnerID === userID);
+        } catch (e) {
+          console.log("results: ", results);
+        }
+      })
+    });
+  }
+  public isListItemOwner = async (userID: number, itemID: number): Promise<boolean> => {
+    return new Promise((resolve, _reject) => {
+      console.log(itemID);
+      return this.listDb.getListItemOwner(itemID, (err, results) => {
+        try {
+          const listOwnerID = results[0].owner_id as unknown as number;
+          return resolve(listOwnerID === userID);
+        } catch (e) {
+          console.log("results: ", results);
+        }
       })
     });
   }
@@ -35,4 +53,5 @@ export class ListService {
       });
     })
   }
+
 }