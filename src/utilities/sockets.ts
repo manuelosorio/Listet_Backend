f20cb049f03cf986d874302528815f7197dc448b
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2022-11-30 20:28:29 -0500

    Update readme, sockets.ts and fixed a search related bug.
    Change to using static methods in the class for socketIO instance instead of exporting functions. Also updated the files that use socket.io server to match the changes.

--- src/utilities/sockets.ts
+++ src/utilities/sockets.ts
@@ -9,70 +9,9 @@ import { ListItemModel } from '../models/list-item.model';
 import { ListEvents } from '../helper/events/list.events';
 import { ListModel } from '../models/list.model';
 
-let socketInstance: SocketIO.Socket;
-let ioInstance: SocketIO.Server;
-
-function setSocketInstance(socket: SocketIO.Socket) {
-  return socketInstance = socket;
-}
-// noinspection JSUnusedGlobalSymbols
-export function getSocketInstance(): SocketIO.Socket {
-  return socketInstance;
-}
-function setIoInstance(io: SocketIO.Server) {
-  return ioInstance = io;
-}
-export function getIoInstance(): SocketIO.Server {
-  return ioInstance;
-}
-
-
-export const emit = (event: string | ListEvents | CommentEvents | ListItemEvents, data: Partial<number | ListModel | ListCommentEmitter | ListItemModel> | any): void => {
-  const io = getIoInstance();
-  try {
-    switch (event) {
-      case ListItemEvents.ADD_ITEM: {
-        io.sockets.to((data as ListItemModel).slug).emit(event, data);
-        break;
-      }
-      case ListItemEvents.COMPLETE_ITEM: {
-        io.sockets.to((data as ListItemModel).slug).emit(event, data);
-        break;
-      }
-      case CommentEvents.UPDATE_COMMENT:
-      case ListItemEvents.UPDATE_ITEM: {
-        io.sockets.emit(event, data);
-        break;
-      }
-      case ListEvents.UPDATE_LIST: {
-        console.log(chalk.bgCyan.black(event, 'emitted'));
-        console.log(data);
-        io.sockets.to((data as ListModel).prevSlug).emit(event, data);
-        break;
-      }
-      case ListItemEvents.DELETE_ITEM:
-      case ListEvents.DELETE_LIST:
-      case CommentEvents.DELETE_COMMENT: {
-        console.log(chalk.bgCyan.black(event, 'emitted'));
-        io.sockets.emit(event, data)
-        break;
-      }
-      default: {
-        console.log(chalk.bgCyan.black(event, 'emitted'));
-        if (typeof data !== 'number') {
-          if ('listInfo' in data) {
-            io.to(data.listInfo).emit(event, data);
-          }
-        }
-        break;
-      }
-    }
-  } catch (e) {
-    console.error(chalk.red(e))
-  }
-}
-
 export class Sockets {
+  private static socketInstance: SocketIO.Socket;
+  private static ioInstance: SocketIO.Server;
   private readonly io: Server;
   constructor(server: http.Server) {
     this.io = new SocketIO.Server(server, ({
@@ -85,12 +24,25 @@ export class Sockets {
       transports: ['polling']
     }));
     console.log(chalk.bgYellow.black('Websocket Initialized!'));
-    setIoInstance(this.io);
+    Sockets.setIoInstance(this.io);
   }
-  connect(): void {
+  private static setSocketInstance(socket: SocketIO.Socket): void {
+    this.socketInstance = socket;
+  }
+
+  public static getSocketInstance(): SocketIO.Socket {
+    return this.socketInstance;
+  }
+  private static setIoInstance(io: SocketIO.Server): void {
+    this.ioInstance = io;
+  }
+  public static getIoInstance(): SocketIO.Server {
+    return this.ioInstance;
+  }
+  public connect(): void {
     this.io.on('connection', async (socket: Socket) => {
       console.log(chalk.bgYellow.black(`Client #${socket.id} Connected.`))
-      setSocketInstance(socket);
+      Sockets.setSocketInstance(socket);
 
       socket.on('join', (res) => {
         console.log(chalk.bgYellow.black(`Client #${socket.id} joined: ${res}  \n`));
@@ -100,5 +52,51 @@ export class Sockets {
         console.log(chalk.bgYellow.black(`Client #${socket.id} Disconnected: ${reason}. \n`))
       });
     })
+  }
+
+
+  public static emit = (event: string | ListEvents | CommentEvents | ListItemEvents, data: Partial<number | ListModel | ListCommentEmitter | ListItemModel> | any): void => {
+    const io: SocketIO.Server = Sockets.ioInstance;
+    try {
+      switch (event) {
+        case ListItemEvents.ADD_ITEM: {
+          io.sockets.to((data as ListItemModel).slug).emit(event, data);
+          break;
+        }
+        case ListItemEvents.COMPLETE_ITEM: {
+          io.sockets.to((data as ListItemModel).slug).emit(event, data);
+          break;
+        }
+        case CommentEvents.UPDATE_COMMENT:
+        case ListItemEvents.UPDATE_ITEM: {
+          io.sockets.emit(event, data);
+          break;
+        }
+        case ListEvents.UPDATE_LIST: {
+          console.log(chalk.bgCyan.black(event, 'emitted'));
+          console.log(data);
+          io.sockets.to((data as ListModel).prevSlug).emit(event, data);
+          break;
+        }
+        case ListItemEvents.DELETE_ITEM:
+        case ListEvents.DELETE_LIST:
+        case CommentEvents.DELETE_COMMENT: {
+          console.log(chalk.bgCyan.black(event, 'emitted'));
+          io.sockets.emit(event, data)
+          break;
+        }
+        default: {
+          console.log(chalk.bgCyan.black(event, 'emitted'));
+          if (typeof data !== 'number') {
+            if ('listInfo' in data) {
+              io.to(data.listInfo).emit(event, data);
+            }
+          }
+          break;
+        }
+      }
+    } catch (e) {
+      console.error(chalk.red(e))
+    }
   }
 }
