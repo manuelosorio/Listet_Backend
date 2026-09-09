1e0246e95c4b93d6939dded36d16b7a6490c494e
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2026-09-09 15:49:12 -0400

    feature: apply IP rate limit to reset-password token route

--- src/api/tokens.api.ts
+++ src/api/tokens.api.ts
@@ -1,13 +1,18 @@
 import { Router } from 'express';
 import { ResetTokenController } from '#controllers/token/reset-token.controller';
 import { VerificationTokenController } from '#controllers/token/verification-token.controller';
+import { ipApiLimiter } from '#middleware/rate-limit.middleware';
 const tokensApi = Router();
 
 const resetToken = new ResetTokenController();
 const verifyToken = new VerificationTokenController();
 
 tokensApi.get('/reset-password/:tokenStore', resetToken.checkToken);
-tokensApi.put('/reset-password/:tokenStore', resetToken.resetPassword);
+tokensApi.put(
+  '/reset-password/:tokenStore',
+  ipApiLimiter,
+  resetToken.resetPassword
+);
 
 tokensApi.get('/verify-account/:tokenStore', verifyToken.verifyAccount);
 export default tokensApi;
